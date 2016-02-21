8f127378a09dea2d795cd9f9ffff1401145d8244
Sprasad <devsprasadbhat@gmail.com>
2016-02-21 19:13:07 +0530

    added Modem immutable type

--- src/modem.jl
+++ src/modem.jl
@@ -1,35 +1,38 @@
 module modem
 
+export Modem, create_modem, modulate, demodulate,display,
+       getbps,getM, destroy, count_biterrors, PSK, DPSK, 
+       ASK, QAM
 
 
-export pskmodem,
-      display,
-      getbps,
-      destroy,
-      modulate,
-      demodulate,
-      MODEM_SCHEME
+const PSK="psk"
+const DPSK="dpsk"
+const ASK="ask"
+const QAM="qam"
 
- #module body
- #
+immutable Modem
+	scheme::AbstractString
+	M::Int
+	modemObjPtr::Ptr{Void}
+end
 
 
-function pskmodem(M::Int)
+function pskmodemPtr(M::Int)
 	scheme = round(Int,log2(M));
 	m = ccall((:modem_create,"libliquid"), Ptr{Void}, (Int32, ), scheme);
 end
 
-function dpskmodem(M::Int)
+function dpskmodemPtr(M::Int)
 	scheme = round(Int,log2(M)) + 8;
 	m = ccall((:modem_create,"libliquid"), Ptr{Void}, (Int32, ), scheme);
 end
 
-function askmodem(M::Int)
+function askmodemPtr(M::Int)
 	scheme = round(Int,log2(M)) + 16;
 	m = ccall((:modem_create,"libliquid"), Ptr{Void}, (Int32, ), scheme);
 end
 
-function qammodem(M::Int)
+function qammodemPtr(M::Int)
 	scheme = round(Int, log2(M))
 	if(scheme <= 1)
 	  error("QAM2 is not possible")
@@ -40,57 +43,98 @@ function qammodem(M::Int)
 	end
 end
 
-@enum MODEM_SCHEME PSK=1 DPSK ASK QAM
 
-function create_modem(scheme::MODEM_SCHEME, M::Int)
-	if(scheme == PSK)
-		m = pskmodem(M);
-	elseif(scheme == ASK)
-		m = askmodem(M);
-	elseif(scheme == DPSK)
-		m = dpskmodem(M);
-	elseif(scheme == QAM)
-		m = qammodem(M);
+
+function create_modem(scheme::AbstractString)
+	scheme = uppercase(scheme);
+	if(scheme == "QPSK")
+		return create_modem(PSK, 4);
+	else
+		try
+			if(startswith(scheme,"PSK"))
+				M = parse(Int, replace(scheme, "PSK", ""));
+				return create_modem(PSK, M);
+			elseif(startswith(scheme, "DPSK"))
+				M = parse(Int, replace(scheme, "DPSK", ""));
+				return create_modem(PSK, M);
+			elseif(startswith(scheme, "ASK"))
+				M = parse(Int, replace(scheme, "ASK", ""));
+				return create_modem(PSK, M);
+			elseif(startswith(scheme, "QAM"))
+				M = parse(Int, replace(scheme, "QAM", ""));
+				return create_modem(PSK, M);
+			else
+				error("unknown modulation scheme : ", scheme );
+			end
+		catch
+			error("unknown modulation scheme : ", scheme);
+		end
 	end
 end
 
-function display(modem::Ptr{Void})
-	ccall((:modem_print, "libliquid"), Void, (Ptr{Void}, ), modem);
+
+function create_modem(scheme::AbstractString, M::Int)
+	if(scheme == PSK)
+		func = pskmodemPtr;
+	elseif(scheme == ASK)
+		func = askmodemPtr;
+	elseif(scheme == DPSK)
+		func = dpskmodemPtr;
+	elseif(scheme == QAM)
+		func = qammodemPtr;
+	else
+		error("unknown modulation scheme : " , scheme);
+	end
+	return Modem(scheme, M, func(M));
 end
 
-function getbps(modem::Ptr{Void})
-	bps = ccall((:modem_get_bps, "libliquid"), Int, (Ptr{Void}, ), modem);
+function display(modem::Modem)
+	ccall((:modem_print, "libliquid"), Void, (Ptr{Void}, ), modem.modemObjPtr);
+end
+
+function getbps(modem::Modem)
+	bps = ccall((:modem_get_bps, "libliquid"), Int, (Ptr{Void}, ), modem.modemObjPtr);
 	return bps;
 end
 
-function destroy(modem::Ptr{Void})
-	ccall((:modem_destroy, "libliquid"), Void, (Ptr{Void}, ), modem);
+function getM(modem::Modem)
+	return modem.M;
 end
 
-function modulate(modem::Ptr{Void} , symbol::Int)
+function destroy(modem::Modem)
+	ccall((:modem_destroy, "libliquid"), Void, (Ptr{Void}, ), modem.modemObjPtr);
+end
+
+function modulate(modem::Modem , symbol::Int)
 	y = Array(Complex64,1);
-	ccall((:modem_modulate, "libliquid"), Void, (Ptr{Void}, Int, Ptr{Complex64}), modem , symbol, y);
+	M = modem.M;
+	if(symbol > (M-1))
+		error("symbol exceeds constellation size (maximum = " , (M-1), ")");
+	end
+	ccall((:modem_modulate, "libliquid"), Void, (Ptr{Void}, Int, Ptr{Complex64}), 
+		modem.modemObjPtr , symbol, y);
 	return y[1];
 end
 
-function modulate(modem::Ptr{Void}, symbols::Array{Int})
+function modulate(modem::Modem, symbols::Array{Int})
 	y = Array(Complex64,length(symbols));
 	for i in range(1, length(symbols))
-		y[i] = modulate(modem, symbols[i]);
+		y[i] = modulate(modem.modemObjPtr, symbols[i]);
 	end
 	return y;
 end
 
-function demodulate(modem::Ptr{Void}, mod::Complex64)
+function demodulate(modem::Modem, mod::Complex64)
 	x = Array(Int, 1);
-	ccall((:modem_demodulate, "libliquid"), Void, (Ptr{Void}, Complex64, Ptr{Int}), modem , mod, x );
+	ccall((:modem_demodulate, "libliquid"), Void, (Ptr{Void}, Complex64, Ptr{Int}), 
+		modem.modemObjPtr , mod, x );
 	return x[1];
 end
 
-function demodulate(modem::Ptr{Void}, mods::Array{Complex64})
+function demodulate(modem::Modem, mods::Array{Complex64})
 	y = Array(Int,length(mods));
 	for i in range(1, length(mods))
-		y[i] = demodulate(modem, mods[i]);
+		y[i] = demodulate(modem.modemObjPtr, mods[i]);
 	end
 	return y;
 end
